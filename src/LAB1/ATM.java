1e557c183a5828e34de182275f4d72014e7478e0
badar24434 <164218215+badar24434@users.noreply.github.com>
2025-10-27 10:14:27 +0800

    first commit

---
+++ src/LAB1/ATM.java
@@ -0,0 +1,13 @@
+package LAB1;
+
+public class ATM {
+    public static void main(String[] args) {
+        System.out.println("==========================================");
+        System.out.println("        Making Money Bank LAB1.ATM Service     ");
+        System.out.println();
+        System.out.println("              Welcome!                    ");
+        System.out.println();
+        System.out.println(" Please insert your ATM card to continue. ");
+        System.out.println("==========================================");
+    }
+}
---
+++ src/LAB1/Student.java
@@ -0,0 +1,11 @@
+package LAB1; //from the folder that you created your java file
+
+// This program prints personal student information
+public class Student {
+    public static void main(String[] args) {
+            System.out.println("Full Name   : Muhamad Badar Miqdad");
+            System.out.println("Matric No   : 215625");
+            System.out.println("Address     : Kolej Canselor, Malaysia");
+        }
+}
+    
