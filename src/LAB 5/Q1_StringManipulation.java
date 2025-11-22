3fc3182f013f72f247994c2065823bcf4d8e94b1
badar24434 <164218215+badar24434@users.noreply.github.com>
2025-11-22 22:19:01 +0800

    Add initial implementation of Q1_StringManipulation class with main method

---
+++ src/LAB 5/Q1_StringManipulation.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Q1_StringManipulation {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+       
+    }
+}
