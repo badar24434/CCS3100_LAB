0cda7b00f0eeedcd2e7b1b846e1cad1b574021a9
badar24434 <164218215+badar24434@users.noreply.github.com>
2025-12-07 16:06:47 +0800

    Remove length check and substring display for first string input

--- src/LAB 5/Q1_StringManipulation.java
+++ src/LAB 5/Q1_StringManipulation.java
@@ -8,13 +8,7 @@ public class Q1_StringManipulation {
         System.out.print("Enter first String : ");
         String firstString = input.nextLine();
         
-        // Check length and display substring if exceeds 10
-        if (firstString.length() > 10) {
-            String substring = firstString.substring(4, 10);
-            System.out.println("Substring from 5 to 10 : " + substring);
-        } else {
-            System.out.println("String length less than 10");
-        }
+        
         
         System.out.println();
         // Accept second string
