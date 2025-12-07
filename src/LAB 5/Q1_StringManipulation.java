8698123b1e254068e8cc888d66825cb9e28282f4
badar24434 <164218215+badar24434@users.noreply.github.com>
2025-12-07 16:07:07 +0800

    Remove check for second string existence and replacement in first string

--- src/LAB 5/Q1_StringManipulation.java
+++ src/LAB 5/Q1_StringManipulation.java
@@ -21,18 +21,7 @@ public class Q1_StringManipulation {
         System.out.print("Enter second String : ");
         String secondString = input.nextLine();
         
-        // Check if second string exists in first string
-        if (firstString.indexOf(secondString) != -1) {
-            System.out.println("Second String exists in First String");
-            System.out.println();
-            
-            // Replace second string with "-replaced-"
-            String newFirstString = firstString.replace(secondString, "-replaced-");
-            System.out.println("New first String : " + newFirstString);
-        } 
-        else {
-            System.out.println("Second String does not exist in First String");
-        }
+        
         
         input.close();
 
